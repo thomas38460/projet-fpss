5feb6660662f7d4bda2c6c800d55865e820bd8e9
Thomas <thomas.girard@lecole-ldlc.com>
2019-12-03 12:31:23 +0100

    ygukyukg

---
+++ Assets/FPS/Prefabs/Level/Signs/clé.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clé : MonoBehaviour
+
+
+{
+
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.name == "Player")
+        {
+            Destroy(door);
+            Destroy(gameObject);
+        }
+
+
+
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
